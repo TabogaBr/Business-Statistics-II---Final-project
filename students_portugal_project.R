f1d7c11373a51af781d20425b483c8bbf9abfc1d
TabogaBr <brenda.taboga7@gmail.com>
2023-11-27 08:54:04 +0100

    Initial commit

---
+++ students_portugal_project.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/brend/Documents/R")
+students_health = read.table("student_portugal.csv", sep=";", header = TRUE)
+attach(students_health)
+# data = students_health[ , c(5, 22, 25, 33)]
+# data
